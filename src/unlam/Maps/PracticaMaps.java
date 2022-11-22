7e8870d245f0f09655d589ca0c4bdf2a75e7632a
ivancarranza12 <82419685+ivancarranza12@users.noreply.github.com>
2022-11-22 13:52:55 -0300

    Primer commit

---
+++ src/unlam/Maps/PracticaMaps.java
@@ -0,0 +1,22 @@
+package unlam.Maps;
+
+import java.util.HashMap;
+
+public class PracticaMaps {
+
+	public static void main(String[] args) {
+		
+		
+		HashMap <Integer,String> jugadores = new HashMap <>();
+		
+		
+		jugadores.put(1, "Dibu");
+		jugadores.put(2,"cuti");
+		jugadores.put(3,"tagliafico");
+		
+		System.out.println(jugadores);
+	
+
+	}
+
+}
